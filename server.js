1342a6ce0e9724b27aeeb4e93419ac884132e4f8
Smeby33 <smebedoh33@gmail.com>
2025-04-02 15:37:31 +0100

    server changer

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://ton-domaine.com']
+    origin: ['http://localhost:3000', 'https://gesworker.vercel.app']
 }));
 
 
