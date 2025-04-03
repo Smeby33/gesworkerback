7cd30eaed5f0ce9f9329f246ac3f509be95c85f4
Smeby33 <smebedoh33@gmail.com>
2025-04-03 14:37:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:"3000', 'https://gesworker.vercel.app'],
     methods:['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 
