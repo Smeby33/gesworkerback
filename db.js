49294b22917783544118e4abcadc369b0b3863ee
Emmanuel (Edoh) SAGOUNAMA-MENSAH <smebedoh33@gmail.com>
2025-05-06 20:04:02 +0100

    Update db.js

--- db.js
+++ db.js
@@ -9,8 +9,9 @@ const db = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10,  // Limite de connexions simultanées
-    queueLimit: 0
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 30000 // 20 secondes timeout
 });
 
 // Vérification de la connexion
